18413b695786aee477f4683894599c9660ce3d3b
Jessie Texier <jessitexier8@gmail.com>
2026-02-10 11:15:27 +0100

    ajout phpstan

--- views/home.php
+++ views/home.php
@@ -133,7 +133,7 @@
         </table>
     </div>
     
-    <footer class="text-center mt-5 text-muted">© 2024 - CENEF - MVC PHP</footer>
+    <footer class="text-center mt-5 text-muted">© 2026 - CENEF - MVC PHP</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
